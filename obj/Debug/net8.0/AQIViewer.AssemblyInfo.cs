d63fc016fbe4c874810efffee760ad6d6cdda7fb
bkhomik <bkhomik.knu.ua>
2024-12-10 15:23:48 +0200

    small bugs fixed

--- obj/Debug/net8.0/AQIViewer.AssemblyInfo.cs
+++ obj/Debug/net8.0/AQIViewer.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AQIViewer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e5b418052f748fd86cd74a7ee55d76e7ac087b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7a1c9de9cda1bba55a766c77a0637cf92ed453")]
 [assembly: System.Reflection.AssemblyProductAttribute("AQIViewer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AQIViewer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
